d996f042c5764a8fac5c913010b502874dc1b0c0
Darya M. <aleksheffer@gmail.com>
2024-07-30 23:48:08 +0300

    Change#1

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import hello from './modules/hello';
+
+console.log(hello);
+
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+
+new AirDatepicker('#datepicker');
